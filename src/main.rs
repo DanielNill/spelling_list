d49d963fd3cd91f00a7a0042fe757f024c7d7d60
DanielN <daniellnill@gmail.com>
2016-12-13 00:26:38 -0800

    basic writing to a file

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,13 @@ fn main() {
         println!("spl only accepts 1 word at a time");
     }
     else {
-        let file = match OpenOptions::new().write(true).open("spelling_list.txt") {
+        let file = match OpenOptions::new().write(true).create(true).append(true).open("spelling_list.txt") {
             Ok(file) => file,
             Err(..) => panic!("Error recording word"),
         };
         let mut writer = BufWriter::new(&file);
-        writer.write_all(args[1].as_bytes()).expect("Error recording word 1");
+        writer.write(args[1].as_bytes()).expect("Error recording word");
+        writer.write(b"\n").expect("Error recording word");
         println!("{} added to your spelling list", args[1]); 
     }
 }
