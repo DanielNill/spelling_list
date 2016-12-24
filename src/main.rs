2018a0215d6e638535b8daae973eb3eb6499c052
DanielN <daniellnill@gmail.com>
2016-12-23 20:32:07 -0500

    use clap instead of std

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
-use std::env;
+extern crate clap;
+
+//use std::env;
 use std::fs::OpenOptions;
 use std::io::BufWriter;
 use std::io::Write;
+use clap::{Arg, App};
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
+    let matches = App::new("Spelling List")
+        .version("0.1.0")
+        .author("Daniel Nill")
+        .about("Keep track of words you can't spell")
+        .arg(Arg::with_name("INPUT")
+             .help("Add a word to your list")
+             .required(true)
+             .index(1))
+        .get_matches();
+
+    let word = matches.value_of("INPUT").unwrap();
     
-    if args.len() > 2 {
-        println!("spl only accepts 1 word at a time");
-    }
-    else {
-        let file = match OpenOptions::new().write(true).create(true).append(true).open("spelling_list.txt") {
-            Ok(file) => file,
-            Err(..) => panic!("Error recording word"),
-        };
-        let mut writer = BufWriter::new(&file);
-        writer.write(args[1].as_bytes()).expect("Error recording word");
-        writer.write(b"\n").expect("Error recording word");
-        println!("{} added to your spelling list", args[1]); 
-    }
+    let file = match OpenOptions::new().write(true).create(true).append(true).open("spelling_list.txt") {
+        Ok(file) => file,
+        Err(..) => panic!("Error recording word"),
+    };
+    let mut writer = BufWriter::new(&file);
+    writer.write(word.as_bytes()).expect("Error recording word");
+    writer.write(b"\n").expect("Error recording word");
+    println!("'{}' added to your spelling list", word); 
 }
